144f118f44ae577a6bf129cd05d9b073dcf843bf
rIpteyed <justin@rich-innovation.com>
2018-04-02 19:28:22 -0700

    first commit.

---
+++ StringUtils.js
@@ -0,0 +1,38 @@
+const readline = require('readline');
+
+String.prototype.isAlpha = function() {
+  var regExp = /^[A-Za-z]+$/;
+  return (this.match(regExp));
+};
+
+
+var interface = readline.createInterface( process.stdin, process.stdout );
+var prompt = function(q) {
+  return new Promise( (resolve, reject) => {
+    interface.question( q, answer => {
+      resolve(answer);
+    })
+  });
+};
+
+(async function main() {
+  var response;
+  while ( response != 'exit' ) {
+    response = await prompt('Please enter a string (alpha only): ');
+    if(response.isAlpha()) {
+      console.log(`Result: ${removeDuplicates(response)}`)
+    } else {
+      console.log('Please try again with alphabetic characters only.')
+    }
+  }
+})();
+
+function removeDuplicates(string) {
+  var unique = '';
+  for (var i = 0; i < string.length; i++) {
+    if(unique.indexOf(string[i].toLowerCase()) < 0 && unique.indexOf(string[i].toUpperCase()) < 0) {
+      unique += string[i];
+    }
+  }
+  return unique;
+}
