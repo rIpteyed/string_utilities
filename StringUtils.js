0b30e7a791473ee124de90ad46d7d249c51f3b2d
rIpteyed <justin@rich-innovation.com>
2018-04-12 17:23:17 -0700

    isAnnagram.

--- StringUtils.js
+++ StringUtils.js
@@ -1,29 +1,30 @@
-const readline = require('readline');
-const removeDuplicates = require('./removeDuplicates');
-
-String.prototype.isAlpha = function() {
-  var regExp = /^[A-Za-z]+$/;
-  return (this.match(regExp));
-};
-
-
-var interface = readline.createInterface( process.stdin, process.stdout );
-var prompt = function(q) {
-  return new Promise( (resolve, reject) => {
-    interface.question( q, answer => {
-      resolve(answer);
-    })
-  });
-};
-
-(async function main() {
-  var response;
-  while ( response != 'exit' ) {
-    response = await prompt('Please enter a string (alpha only): ');
-    if(response.isAlpha()) {
-      console.log(`Result: ${removeDuplicates(response)}`)
-    } else {
-      console.log('Please try again with alphabetic characters only.')
+var removeDuplicates = function (string) {
+  var unique = '';
+  for (var i = 0; i < string.length; i++) {
+    if(unique.indexOf(string[i].toLowerCase()) < 0 && unique.indexOf(string[i].toUpperCase()) < 0) {
+      unique += string[i];
     }
   }
-})();
+  return unique;
+}
+
+var isAnnagram = function (words) {
+  var resultString = '',
+    a = removeDuplicates(words[0]),
+    b = removeDuplicates(words[1]);
+
+  // Remove the possibility that duplicated characters will return false positive
+  if(a.length != b.length || words[0].length != words[1].length) {
+    return false;
+  }
+
+  for(const letter of words[0]) {
+    const position = words[1].indexOf(letter);
+    if(position >= 0) {
+      resultString += letter;
+    }
+  }
+  return resultString === words[0];
+}
+
+module.exports = { removeDuplicates, isAnnagram };
---
+++ index.js
@@ -0,0 +1,69 @@
+const readline = require('readline');
+const { removeDuplicates, isAnnagram } = require('./StringUtils');
+
+String.prototype.isAlpha = function() {
+  var regExp = /^[A-Za-z]+$/;
+  return (this.match(regExp));
+};
+
+console.log(`\n>>>>>>>> Welcome to String Utilities <<<<<<<<\n`);
+
+var interface = readline.createInterface( process.stdin, process.stdout );
+var prompt = function(q) {
+  return new Promise( (resolve, reject) => {
+    interface.question( q, answer => {
+      if(answer !== 'x') {
+        resolve(answer);
+      } else {
+        interface.close();
+      }
+    })
+  });
+};
+
+(async function main() {
+  var type = '';
+  while(type !== 'x') {
+    type = await prompt('Options:\n\n[1]-removeDuplicates\n[2]-isAnnagram\n[x] to exit.\n\n');
+    switch (type) {
+      case '1':
+        await tryRemoveDuplicates();
+        break;
+      case '2':
+        await tryIsAnnagram();
+        break;
+      default:
+        break;
+    }
+  }
+})();
+
+async function tryRemoveDuplicates() {
+  var response = '';
+  while (response !== 'quit') {
+    response = await prompt('Please enter a string (alpha only) or [quit]:\n');
+    if(response.isAlpha()) {
+      console.log(`\nResult: ${removeDuplicates(response)}\n`)
+    } else {
+      console.log('\n!! Please try again with alphabetic characters only !!\n')
+    }
+  }
+}
+
+async function tryIsAnnagram() {
+  var response = '';
+  while (response != 'quit') {
+    response = await prompt('To test for annagrams enter two words with a space in between or [quit]:\n'),
+      words = response.split(' ');
+
+    if(words.length != 2) {
+      console.error('\n!! You must enter two words with a space in between !!\n');
+    } else {
+      console.log(`Result: ${isAnnagram(words)}`);
+    }
+  }
+}
+
+process.on('exit', function(code) {
+  return console.log(`>>>>>>>> Thank you for using String Utilities <<<<<<<<\n\n`);
+});
